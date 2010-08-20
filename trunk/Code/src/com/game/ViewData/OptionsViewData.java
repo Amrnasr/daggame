10af73169ca6a81d9293dccd9922ed08f8ea6793
dark.ying <dark.ying@87c192f6-a8a6-a0b6-c7b9-b717c2d47c41>
2010-08-20 12:00:11 +0000

    Ying: Finshed spinners in "options" and now need more work

--- trunk/Code/src/com/game/ViewData/OptionsViewData.java
+++ trunk/Code/src/com/game/ViewData/OptionsViewData.java
@@ -1,6 +1,7 @@
 package com.game.ViewData;
 
 
+import com.game.MsgType;
 import com.game.R;
 
 import android.app.Activity;
@@ -8,6 +9,10 @@ import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Specific view data class for the "Options" screen.
@@ -30,7 +35,31 @@ public class OptionsViewData extends ViewData
         View xmlLayout = (View) li.inflate(R.layout.options, null);
         
         // Callback for the buttons & views
-       
+        
+        Button okButton = (Button) xmlLayout.findViewById(R.id.ok_options_but);
+        okButton.setOnClickListener(new OnClickListener() 
+        {
+          @Override
+          public void onClick(View v) {
+            Log.i("ViewData", " Clicked OK button");
+            handlerRef.sendMessage(handlerRef.obtainMessage(MsgType.BUTTON_CLICK.ordinal(), R.id.ok_options_but, 0));
+          }
+        });
+        
+        Spinner numberUnitsSpin = (Spinner) xmlLayout.findViewById(R.id.num_units_spin);
+        ArrayAdapter numberUnitsAdapter = ArrayAdapter.createFromResource(	activity, R.array.count_names, android.R.layout.simple_spinner_item);
+        numberUnitsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        numberUnitsSpin.setAdapter(numberUnitsAdapter);
+        
+        Spinner moveSpeedSpin = (Spinner) xmlLayout.findViewById(R.id.move_speed_spin);
+        ArrayAdapter moveSpeedAdapter = ArrayAdapter.createFromResource(	activity, R.array.speed_names, android.R.layout.simple_spinner_item);
+        moveSpeedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        moveSpeedSpin.setAdapter(moveSpeedAdapter);
+        
+        Spinner eatSpeedSpin = (Spinner) xmlLayout.findViewById(R.id.eat_speed_spin);
+        ArrayAdapter eatSpeedAdapter = ArrayAdapter.createFromResource(	activity, R.array.speed_names, android.R.layout.simple_spinner_item);
+        eatSpeedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        eatSpeedSpin.setAdapter(eatSpeedAdapter);
         
 		return xmlLayout;
 	}
