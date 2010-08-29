b085f558563c12b5dbc276ade6139a988c216e65
dark.ying <dark.ying@87c192f6-a8a6-a0b6-c7b9-b717c2d47c41>
2010-08-29 15:23:32 +0000

    Ying: Added tile class, as well as constants and player

---
+++ Code/src/com/game/Constants.java
@@ -0,0 +1,9 @@
+package com.game;
+
+public class Constants 
+{
+	private Constants() {}
+	
+	public static final int MaxPlayers = 6;
+
+}
---
+++ Code/src/com/game/Player.java
@@ -0,0 +1,10 @@
+package com.game;
+
+/**
+ * Stub class so I can create the Tiles
+ * @author Ying
+ *
+ */
+public class Player {
+
+}
