061637626ccfc9804198c0d3e142ffc7355dc04f
dark.ying <dark.ying@87c192f6-a8a6-a0b6-c7b9-b717c2d47c41>
2010-09-04 21:17:32 +0000

    Ying: BallInputDevice and TrackballInputDevice working now

--- trunk/Code/src/com/game/InputDevice/BallInputDevice.java
+++ trunk/Code/src/com/game/InputDevice/BallInputDevice.java
@@ -9,23 +9,32 @@ import android.util.Log;
 import android.view.MotionEvent;
 
 
-
+/**
+ * Class to handle the input of a player using the trackball
+ * @author Ying
+ *
+ */
 public class BallInputDevice extends InputDevice 
 {
 
+	/**
+	 * Creates the Handler and asigns it.
+	 * @param playScene
+	 */
 	public BallInputDevice(PlayScene playScene)
 	{
 		super(playScene);
 		
 		this.deviceHandler = new Handler()
-		{
-			
-			public void HandleMessage(Message msg)
+		{			
+			public void handleMessage(Message msg)
 			{
 				if(msg.what == MsgType.TRACKBALL_EVENT.ordinal())
 				{
 					MotionEvent event = (MotionEvent)msg.obj;
 					Log.i("BallInputDevice", "Trackball event: " + event.getX() + ", " + event.getY());
+					
+					// TODO: Transform trackball input into cursor movement
 				}
 			}
 		};
--- trunk/Code/src/com/game/Map.java
+++ trunk/Code/src/com/game/Map.java
@@ -34,16 +34,13 @@ public class Map {
 		Log.i("Map", "Started constructor");
 		mBitmap=BitmapFactory.decodeResource(activity.getResources(), mapRef);
 		//mBitmap=BitmapFactory.decodeFile("res/drawable/map_size480_1.png");
-		Log.i("Map", "a");
 		int tilesPerRow = mBitmap.getWidth() / Constants.TileWidth;
 		int tilesPerColumn = mBitmap.getHeight() / Constants.TileWidth;
-		Log.i("Map", "b");
 		//Initialize the matrix
 		mTileMap = new Vector<Vector<Tile>>();
 		for(int i = 0; i < tilesPerRow; i++){
 			mTileMap.addElement(new Vector<Tile>());
 		}
-		Log.i("Map", "c");
 		
 		//Calculate the maximum capacity of each tile and initialize them
 		Iterator<Vector<Tile>> it = mTileMap.listIterator();
@@ -62,7 +59,6 @@ public class Map {
 				tileVector.addElement(new Tile(i,j,whitePixels));
 			}
 		}
-		Log.i("Map", "d");
 	}
 	
 	public Bitmap getBitmap()
