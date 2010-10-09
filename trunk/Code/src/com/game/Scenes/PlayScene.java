e5de2ae7421dc8b5353f93484aedbab1d3e05cea
finalholocaust <finalholocaust@87c192f6-a8a6-a0b6-c7b9-b717c2d47c41>
2010-10-09 19:01:44 +0000

    -Added the preprocessing of the tile map, including the program that generates the files (in the tools folder) -Improved documentation and some unnecessary variables removed

--- trunk/Code/src/com/game/Scenes/PlayScene.java
+++ trunk/Code/src/com/game/Scenes/PlayScene.java
@@ -191,7 +191,7 @@ public class PlayScene extends Scene
 		{
             public void run() {
             	Log.i("Debug", "Calling Playscene new thread run");
-            	map = new Map(refActivity,R.drawable.map_size480_1);
+            	map = new Map(refActivity,R.drawable.samplemap, R.raw.samplemaptilemap);
             	MessageHandler.Get().Send(MsgReceiver.ACTIVITY, MsgType.ACTIVITY_DISMISS_LOAD_DIALOG);            	
             	mapLoaded = true;
 
@@ -199,8 +199,8 @@ public class PlayScene extends Scene
 	            	MessageHandler.Get().Send(
 	            			MsgReceiver.RENDERER, 
 	            			MsgType.NEW_TILEMAP, 
-	            			map.getBitmap().getWidth()/Constants.TileWidth,
-	            			map.getBitmap().getHeight()/Constants.TileWidth,  
+	            			0,
+	            			0,  
 	            			map.getTileMap());        			
         		}
         		else{
--- trunk/Code/src/com/game/Tile.java
+++ trunk/Code/src/com/game/Tile.java
@@ -11,11 +11,28 @@ import android.graphics.Point;
  */
 public class Tile 
 {
+	/**
+	 * Tile's Maximum capacity
+	 */
 	public int maxCapacity;
+	/**
+	 * Amount of each player's units in the tile 
+	 */
 	private Vector<Integer> density;
+	/**
+	 * Reference to the players with units in the tile
+	 */
 	private Vector<Player> players;
+	/**
+	 * Tile's position
+	 */
 	private Point position;
-	
+	/**
+	 * Initializes the tile
+	 * @param x coordinates
+	 * @param y coordinates
+	 * @param maxCapacity Tile's maximum capacity
+	 */
 	public Tile(int x, int y, int maxCapacity)
 	{
 		this.density = new Vector<Integer>(Constants.MaxPlayers);
