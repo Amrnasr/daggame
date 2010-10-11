ad16b204c96f11929be3ba514208d17014bc0393
dark.ying <dark.ying@87c192f6-a8a6-a0b6-c7b9-b717c2d47c41>
2010-10-11 08:22:21 +0000

    Ying: Documented

--- trunk/Code/src/com/game/RenderInitData.java
+++ trunk/Code/src/com/game/RenderInitData.java
@@ -4,12 +4,31 @@ import java.util.Vector;
 
 import android.graphics.Bitmap;
 
+/**
+ * Data packet class for initializing the renderer.
+ * @author Ying
+ *
+ */
 public class RenderInitData 
 {
+	/**
+	 * Map texture
+	 */
 	private Bitmap mapImage;
+	
+	/**
+	 * Tilemap data
+	 */
 	private Vector<Tile> tileMap;
+	
+	/**
+	 * Player cursor vector
+	 */
 	private Vector<Cursor> cursors;
 	
+	/**
+	 * Creates a instance of the RenderInitData class
+	 */
 	public RenderInitData()
 	{
 		this.mapImage = null;
@@ -17,11 +36,39 @@ public class RenderInitData
 		this.cursors = null;
 	}
 	
+	/**
+	 * Sets the cursors vector
+	 * @param cursors
+	 */
 	public void SetCursors(Vector<Cursor> cursors) { this.cursors = cursors; }
+	
+	/**
+	 * Sets the tile vector
+	 * @param tileMap
+	 */
 	public void SetTileMap(Vector<Tile> tileMap) { this.tileMap = tileMap; }
+	
+	/**
+	 * Sets the map texture
+	 * @param mapImage
+	 */
 	public void SetMapImage(Bitmap mapImage) { this.mapImage = mapImage; }
 	
+	/**
+	 * Gets the cursor vector.
+	 * @return
+	 */
 	public Vector<Cursor> GetCursors() { return this.cursors; }
+	
+	/**
+	 * Gets the vector of tiles
+	 * @return
+	 */
 	public Vector<Tile> GetTileMap() { return this.tileMap; }
+	
+	/**
+	 * Gets the map texture
+	 * @return
+	 */
 	public Bitmap GetMapImage() { return this.mapImage; }
 }
